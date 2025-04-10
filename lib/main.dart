245c9ecdeefd57c37cb8934beb3031cef928492f
Bartosz Siedlecki <65456083+Monterisko@users.noreply.github.com>
2025-04-10 16:20:19 +0200

    Dodanie bazy danych

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:grand_chess/firebase_options.dart';
 import 'package:grand_chess/pages/HomePage.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
